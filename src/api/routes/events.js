668e3a53fe476c68e56ffeccfd4e8e3441f406c3
marycala <marycala87@gmail.com>
2025-06-30 10:50:08 +0200

    Permitir upload de imágenes en ruta PUT /events/:id con upload.fields

--- src/api/routes/events.js
+++ src/api/routes/events.js
@@ -17,7 +17,9 @@ eventsRouter.get('/:id', getEventById);
 eventsRouter.get('/search/title/:title', getEventByTitle);
 eventsRouter.get('/search/date/:date', getEventByDate);
 eventsRouter.post('/create', isAuth, upload.single('img'), createEvent);
-eventsRouter.put('/:id', isAuth, upload.single('img'), updateEvent);
+eventsRouter.put('/:id', isAuth, upload.fields([
+  { name: 'img', maxCount: 1 }
+]), updateEvent);
 eventsRouter.delete('/:id', isAuth, deleteEvent);
 
 module.exports = eventsRouter;
