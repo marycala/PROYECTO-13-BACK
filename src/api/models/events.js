afd6b5d1ee20e367f0f64adec1e858245ae3b152
marycala <marycala87@gmail.com>
2025-06-30 12:40:45 +0200

    Actualizar el esquema de eventos para que attendees referencie a users

--- src/api/models/events.js
+++ src/api/models/events.js
@@ -20,7 +20,7 @@ const eventSchema = new mongoose.Schema(
     attendees: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'attendees'
+        ref: 'users'
       }
     ]
   },
